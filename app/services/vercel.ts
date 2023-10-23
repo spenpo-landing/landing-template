c35625d40bb8bc08544685a80a7453a098cacdb3
spope <spope@blockchains.com>
2023-10-23 07:39:08 -0600

    add team id to vercel service

--- app/services/vercel.ts
+++ app/services/vercel.ts
@@ -14,28 +14,37 @@ const headers = {
 };
 
 const getProjectDeployments = async (app: string) =>
-  fetch(`https://api.vercel.com/v2/deployments?app=${app}`, {
-    headers,
-    method: "get",
-  });
+  fetch(
+    `https://api.vercel.com/v2/deployments?app=${app}&teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
+    {
+      headers,
+      method: "get",
+    }
+  );
 
 const getDeploymentAliases = async (deploymentId: string) =>
-  fetch(`https://api.vercel.com/v2/deployments/${deploymentId}/aliases`, {
-    headers,
-    method: "get",
-  });
+  fetch(
+    `https://api.vercel.com/v2/deployments/${deploymentId}/aliases?teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
+    {
+      headers,
+      method: "get",
+    }
+  );
 
 const redeployProject = async (deploymentId: string, name: string) =>
-  fetch("https://api.vercel.com/v13/deployments?forceNew=1", {
-    body: JSON.stringify({
-      name,
-      deploymentId,
-      meta: { action: "redeploy" },
-      target: "production",
-    }),
-    headers,
-    method: "post",
-  });
+  fetch(
+    `https://api.vercel.com/v13/deployments?forceNew=1?teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
+    {
+      body: JSON.stringify({
+        name,
+        deploymentId,
+        meta: { action: "redeploy" },
+        target: "production",
+      }),
+      headers,
+      method: "post",
+    }
+  );
 
 const cancelDeployment = async (deploymentId: string) =>
   fetch(`https://api.vercel.com/v12/deployments/${deploymentId}/cancel`, {
@@ -47,21 +56,27 @@ const addEnvironmentVariables = async (
   projectName: string,
   variables: ProjectEnvVariableInput[]
 ) =>
-  fetch(`https://api.vercel.com/v10/projects/${projectName}/env?upsert=true`, {
-    body: JSON.stringify(variables),
-    headers,
-    method: "post",
-  });
+  fetch(
+    `https://api.vercel.com/v10/projects/${projectName}/env?upsert=true&teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
+    {
+      body: JSON.stringify(variables),
+      headers,
+      method: "post",
+    }
+  );
 
 const getDeployment = async (deploymentId: string) =>
-  fetch(`https://api.vercel.com/v13/deployments/${deploymentId}`, {
-    headers,
-    method: "get",
-  });
+  fetch(
+    `https://api.vercel.com/v13/deployments/${deploymentId}?teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
+    {
+      headers,
+      method: "get",
+    }
+  );
 
 const getDeploymentEvents = async (deploymentId: string) =>
   fetch(
-    `https://api.vercel.com/v2/deployments/${deploymentId}/events?builds=1&direction=forward&follow=1`,
+    `https://api.vercel.com/v2/deployments/${deploymentId}/events?builds=1&direction=forward&follow=1&teamId=${process.env.NEXT_PUBLIC_VERCEL_TEAM}`,
     {
       headers,
       method: "get",
