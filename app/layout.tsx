5655231ef0dafbbf17b2def013d467e1d206c4fe
Spencer Pope <spenpo@spenpo.com>
2024-01-24 19:10:54 -0700

    defualt.svg image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,19 @@ import { ThemeProvider } from './context/themeProvider'
 import { CssBaseline } from '@mui/material'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 
+export function generateViewport() {
+  return {
+    themeColor: process.env.NEXT_PUBLIC_ACCENT_COLOR,
+    viewport: {
+      width: 'device-width',
+      initialScale: 1,
+      maximumScale: 1,
+    },
+  }
+}
+
 export const metadata: Metadata = {
+  metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: process.env.NEXT_PUBLIC_NAME,
   description: process.env.NEXT_PUBLIC_TITLE,
   openGraph: {
@@ -14,13 +26,7 @@ export const metadata: Metadata = {
     description: process.env.NEXT_PUBLIC_TITLE,
     url: process.env.VERCEL_URL,
     siteName: process.env.NEXT_PUBLIC_NAME,
-    images: [
-      {
-        url: process.env.OG_IMAGE || '',
-        width: 800,
-        height: 600,
-      },
-    ],
+    images: [`${process.env.OG_IMAGE}`, '/default.svg'],
     locale: 'en_US',
     type: 'website',
   },
@@ -38,26 +44,20 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: process.env.NEXT_PUBLIC_HEADSHOT || '',
-    shortcut: process.env.NEXT_PUBLIC_HEADSHOT || '',
-    apple: process.env.NEXT_PUBLIC_HEADSHOT || '',
+    icon: `${process.env.NEXT_PUBLIC_HEADSHOT}`,
+    shortcut: `${process.env.NEXT_PUBLIC_HEADSHOT}`,
+    apple: `${process.env.NEXT_PUBLIC_HEADSHOT}`,
     other: {
       rel: 'apple-touch-icon-precomposed',
-      url: process.env.NEXT_PUBLIC_HEADSHOT || '',
+      url: `${process.env.NEXT_PUBLIC_HEADSHOT}`,
     },
   },
-  themeColor: process.env.NEXT_PUBLIC_ACCENT_COLOR,
   twitter: {
     card: 'summary_large_image',
     title: process.env.NEXT_PUBLIC_NAME,
     description: process.env.NEXT_PUBLIC_TITLE,
     creator: '@s_pop3',
-    images: [process.env.OG_IMAGE || ''],
-  },
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
+    images: [`${process.env.OG_IMAGE}`, '/default.svg'],
   },
 }
 
